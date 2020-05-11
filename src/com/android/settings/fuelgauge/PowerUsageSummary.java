16bc5729ddee3563e8cad4216da8a844d97db4a4
SuperDroidBond <superdroidbond@yahoo.com>
2020-05-11 22:36:36 +0530

    BatteryHealth: show Charge Cycles Completed
    Overlay -:
        <!-- Battery Health -->
        <string name="config_batChargeCycle">/sys/class/power_supply/bms/cycle_count</string>
    
    Change-Id: If07ee074ad5021b7fef0ecfff086e4d5d7249e70
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- src/com/android/settings/fuelgauge/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -78,9 +78,11 @@ public class PowerUsageSummary extends PowerUsageBase implements
     static final String KEY_BATTERY_USAGE = "battery_usage_summary";
     private static final String KEY_CURRENT_BATTERY_CAPACITY = "current_battery_capacity";
     private static final String KEY_DESIGNED_BATTERY_CAPACITY = "designed_battery_capacity";
+    private static final String KEY_BATTERY_CHARGE_CYCLES = "battery_charge_cycles";
 
     private String mBatDesCap;
     private String mBatCurCap;
+    private String mBatChgCyc;
 
     private static final String KEY_BATTERY_TEMP = "battery_temperature";
 
@@ -97,6 +99,8 @@ public class PowerUsageSummary extends PowerUsageBase implements
     PowerGaugePreference mCurrentBatteryCapacity;
     @VisibleForTesting
     PowerGaugePreference mDesignedBatteryCapacity;
+    @VisibleForTesting
+    PowerGaugePreference mBatteryChargeCycles;
     @VisibleForTesting
     PowerUsageFeatureProvider mPowerFeatureProvider;
     @VisibleForTesting
@@ -200,6 +204,8 @@ public class PowerUsageSummary extends PowerUsageBase implements
                 KEY_CURRENT_BATTERY_CAPACITY);
         mDesignedBatteryCapacity = (PowerGaugePreference) findPreference(
                 KEY_DESIGNED_BATTERY_CAPACITY);
+        mBatteryChargeCycles = (PowerGaugePreference) findPreference(
+                KEY_BATTERY_CHARGE_CYCLES);
         mBatteryUtils = BatteryUtils.getInstance(getContext());
 
         if (Utils.isBatteryPresent(getContext())) {
@@ -333,10 +339,10 @@ public class PowerUsageSummary extends PowerUsageBase implements
 
 	mBatDesCap = getResources().getString(R.string.config_batDesCap);
 	mBatCurCap = getResources().getString(R.string.config_batCurCap);
+        mBatChgCyc = getResources().getString(R.string.config_batChargeCycle);
 
         // reload BatteryInfo and updateUI
         restartBatteryInfoLoader();
-
         if (BatteryInfo.batteryTemp != 0f) {
             mBatteryTempPref.setSubtitle(BatteryInfo.batteryTemp / 10 + " °C");
         } else {
@@ -344,6 +350,7 @@ public class PowerUsageSummary extends PowerUsageBase implements
         }
 	mCurrentBatteryCapacity.setSubtitle(parseBatterymAhText(mBatCurCap));
 	mDesignedBatteryCapacity.setSubtitle(parseBatterymAhText(mBatDesCap));
+	mBatteryChargeCycles.setSubtitle(parseBatteryCycle(mBatChgCyc));
     }
 
     @VisibleForTesting
@@ -423,6 +430,19 @@ public class PowerUsageSummary extends PowerUsageBase implements
                     + file, ioe);
         } catch (NumberFormatException nfe) {
             Log.e(TAG, "Read a badly formatted battery capacity from "
+                    + file, nfe);
+        }
+        return getResources().getString(R.string.status_unavailable);
+    }
+
+    private String parseBatteryCycle(String file) {
+        try {
+            return Integer.parseInt(readLine(file)) + " Cycles";
+        } catch (IOException ioe) {
+            Log.e(TAG, "Cannot read battery cycle from "
+                    + file, ioe);
+        } catch (NumberFormatException nfe) {
+            Log.e(TAG, "Read a badly formatted battery cycle from "
                     + file, nfe);
         }
         return getResources().getString(R.string.status_unavailable);
