AICP/packages_apps_Settings

GestureNav: fix backregion height default
Change-Id: I6e8a10c735a18f82dd70297e65cb2642b784f9c8

src/com/android/settings/gestures/GestureNavigationSettingsFragment.java
@@ -131,7 +131,7 @@ public class GestureNavigationSettingsFragment extends DashboardFragment {
                 ? Settings.Secure.BACK_GESTURE_INSET_SCALE_LEFT
                 : Settings.Secure.BACK_GESTURE_INSET_SCALE_RIGHT);
         float initScale = Settings.Secure.getFloat(
-                getContext().getContentResolver(), settingsKey, 1.0f);
+                getContext().getContentResolver(), settingsKey, key == BACK_REGION_SEEKBAR_KEY ? 0.0f : 1.0f);
 
         // needed if we just change the height
         float currentWidthScale = Settings.Secure.getFloat(

AOSP-Silicon/superior_packages_apps_Settings

Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

src/com/android/settings/gestures/GestureNavigationSettingsFragment.java
@@ -158,7 +158,7 @@ public class GestureNavigationSettingsFragment extends DashboardFragment {
         float initScale = 0;
         if (settingsKey != "") {
             initScale = Settings.Secure.getFloat(
-                  getContext().getContentResolver(), settingsKey, 1.0f);
+                  getContext().getContentResolver(), settingsKey, key == KEY_BACK_HEIGHT ? 0.0f : 1.0f);
         }
 
         // needed if we just change the height