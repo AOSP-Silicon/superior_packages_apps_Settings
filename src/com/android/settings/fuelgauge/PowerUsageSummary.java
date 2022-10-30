Project-Xtended/packages_apps_Settings

BatteryHealth: make resources overlayable
Not every device have the charge full & charge full design files at the same location or same name.
Better to keep it overlayable for maintainer to pick.
Overlay would be for Settings - overlay/packages/apps/Settings/res/values/x_config.xml

For Example -:

    <!-- For OnePlus 3/T -->
    <!-- Battery Health -->
    <string name="config_batDesCap">/sys/class/power_supply/bms/charge_full_design</string>
    <string name="config_batCurCap">/sys/class/power_supply/bms/charge_full</string>

Change-Id: I1ea1fae71064fe072d03013542927f2df2e0c319

src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -106,12 +106,9 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
     private static final String KEY_BATTERY_SAVER_SUMMARY = "battery_saver_summary";
     private static final String KEY_ULTRA_POWER_SAVING = "ultra_power_save";
 
-    private static final String FILENAME_BATTERY_DESIGN_CAPACITY =
-            "/sys/class/power_supply/bms/charge_full_design";
-    private static final String FILENAME_BATTERY_CURRENT_CAPACITY =
-            "/sys/class/power_supply/bms/charge_full";
-
     private SystemSettingSwitchPreference mUltraPower;
+    private String mBatDesCap;
+    private String mBatCurCap;
 
     @VisibleForTesting
     static final int BATTERY_INFO_LOADER = 1;
@@ -317,7 +314,7 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
         }
         return super.onPreferenceTreeClick(preference);
     }
-	
+
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
@@ -434,14 +431,17 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
             mNeedUpdateBatteryTip = true;
         }
 
+        mBatDesCap = getResources().getString(R.string.config_batDesCap);
+        mBatCurCap = getResources().getString(R.string.config_batCurCap);
+
         // reload BatteryInfo and updateUI
         restartBatteryInfoLoader();
         updateLastFullChargePreference();
         mScreenUsagePref.setSummary(StringUtil.formatElapsedTime(getContext(),
                 mBatteryUtils.calculateScreenUsageTime(mStatsHelper), false));
         mBatteryTempPref.setSubtitle(BatteryInfo.batteryTemp+" "+Character.toString ((char) 176) + "C");
-        mCurrentBatteryCapacity.setSubtitle(parseBatterymAhText(FILENAME_BATTERY_CURRENT_CAPACITY));
-        mDesignedBatteryCapacity.setSubtitle(parseBatterymAhText(FILENAME_BATTERY_DESIGN_CAPACITY));
+        mCurrentBatteryCapacity.setSubtitle(parseBatteryCurrentData(mBatCurCap));
+        mDesignedBatteryCapacity.setSubtitle(parseBatteryCurrentData(mBatDesCap));
         final long elapsedRealtimeUs = SystemClock.elapsedRealtime() * 1000;
         Intent batteryBroadcast = context.registerReceiver(null,
                 new IntentFilter(Intent.ACTION_BATTERY_CHANGED));

AOSP-Silicon/superior_packages_apps_Settings

    <string name="config_batDesCap">/sys/class/power_supply/bms/charge_full</string>
    <string name="config_batCurCap">/sys/class/power_supply/bms/charge_now_raw</string>
Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>
Signed-off-by: spezi77 <spezi7713@gmx.net>
Signed-off-by: Joey Huab <joey@evolution-x.org>
Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -79,10 +79,8 @@ public class PowerUsageSummary extends PowerUsageBase implements
     private static final String KEY_CURRENT_BATTERY_CAPACITY = "current_battery_capacity";
     private static final String KEY_DESIGNED_BATTERY_CAPACITY = "designed_battery_capacity";
 
-    private static final String FILENAME_BATTERY_DESIGN_CAPACITY =
-            "/sys/class/power_supply/bms/charge_full_design";
-    private static final String FILENAME_BATTERY_CURRENT_CAPACITY =
-            "/sys/class/power_supply/bms/charge_full";
+    private String mBatDesCap;
+    private String mBatCurCap;
 
     private static final String KEY_BATTERY_TEMP = "battery_temperature";
 
@@ -332,6 +330,10 @@ public class PowerUsageSummary extends PowerUsageBase implements
         } else {
             mNeedUpdateBatteryTip = true;
         }
+
+	mBatDesCap = getResources().getString(R.string.config_batDesCap);
+	mBatCurCap = getResources().getString(R.string.config_batCurCap);
+
         // reload BatteryInfo and updateUI
         restartBatteryInfoLoader();
 
@@ -340,9 +342,9 @@ public class PowerUsageSummary extends PowerUsageBase implements
         } else {
             mBatteryTempPref.setSubtitle(getResources().getString(R.string.status_unavailable));
         }
-	mCurrentBatteryCapacity.setSubtitle(parseBatterymAhText(FILENAME_BATTERY_CURRENT_CAPACITY));
-	mDesignedBatteryCapacity.setSubtitle(parseBatterymAhText(FILENAME_BATTERY_DESIGN_CAPACITY));
-}
+	mCurrentBatteryCapacity.setSubtitle(parseBatterymAhText(mBatCurCap));
+	mDesignedBatteryCapacity.setSubtitle(parseBatterymAhText(mBatDesCap));
+    }
 
     @VisibleForTesting
     void restartBatteryTipLoader() {